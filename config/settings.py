f22adfc304bd025548ed71cef808b1bd3ae43c37
Aravind Hari <avdhari114@gmail.com>
2022-05-14 18:23:29 +0530

    allowed hosts from env

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 
 # Application definition
