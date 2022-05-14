affbd82d26078bfe4d9c850ef0bb8b22555377ad
Aravind Hari <avdhari114@gmail.com>
2022-05-14 17:21:12 +0530

    default SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -24,10 +24,12 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
+    
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("DJANGO_SECRET_KEY")
-
+SECRET_KEY = SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="django-insecure-!c!=ca2v=xm2dnymuh3yjk1vtyfqw#5#vy*ox)tx3q6xdr^4z#",
+)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
