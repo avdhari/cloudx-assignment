5afc0b4a0134470b706d7667a64698ba28652ed3
Aravind Hari <avdhari114@gmail.com>
2022-05-14 18:07:51 +0530

    views init

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from contact.models import UserData
 
-# Create your views here.
+
+def home_view(request):
+    data_list = UserData.objects.all()
+    context = {
+        'data_list': data_list,
+    }
+    return render(request, 'contact/home.html', context)
