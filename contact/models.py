2cb71537e520f624fdcf51f16ff74e93a03f7bcd
Aravind Hari <avdhari114@gmail.com>
2022-05-14 18:40:26 +0530

    fix in static config, str method for models

--- contact/models.py
+++ contact/models.py
@@ -12,3 +12,6 @@ class UserData(BaseModel):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
